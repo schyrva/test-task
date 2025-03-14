d58656be1b51f531aef9e68b03d47e04ee5e1dec
Stanislav Chyrva <alkefirko@gmail.com>
2025-03-14 20:08:25 +0200

    Update build configuration and documentation

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,27 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/experimental-addon-test",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  core: {
+    disableTelemetry: true,
+  },
+  async viteFinal(config) {
+    // Add SVGR to Storybook Vite config
+    return mergeConfig(config, {
+      plugins: [svgr()],
+    });
+  },
 };
 export default config;
