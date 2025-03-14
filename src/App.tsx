9c533d0c7074cd791f92e7c5815c9f6e3549b86e
Stanislav Chyrva <alkefirko@gmail.com>
2025-03-14 20:08:16 +0200

    Update main App to integrate Pokemon team builder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import React from "react";
+import PokemonForm from "./components/Form/PokemonForm";
+import lunaEdgeLogo from "./assets/logos/LunaEdgeLogo.svg";
 
 function App() {
   return (
-    <div>
-      <p className='text-10xl font-bold'>Welcome to Luna Edge technical interview</p>
+    <div className="min-h-screen bg-gray-100 flex flex-col py-12 px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto w-full max-w-md mb-8 flex justify-center bg-indigo-700 p-4 rounded-lg">
+        <img src={lunaEdgeLogo} alt="Luna Edge Logo" className="h-8" />
+      </div>
+      <PokemonForm />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
