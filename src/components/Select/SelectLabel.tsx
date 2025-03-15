3d8dc3c4f7ceb6816f2f1a240f9e5a43704a4166
Stanislav Chyrva <alkefirko@gmail.com>
2025-03-15 23:16:32 +0200

    fix select label style

--- src/components/Select/SelectLabel.tsx
+++ src/components/Select/SelectLabel.tsx
@@ -8,8 +8,11 @@ interface SelectLabelProps {
 
 const SelectLabel: React.FC<SelectLabelProps> = ({ id, label, required }) => {
   return (
-    <label htmlFor={id} className="flex justify-between mb-2">
-      <span className="block text-sm font-medium text-gray-700">
+    <label
+      htmlFor={id}
+      className="flex justify-between text-sm font-medium text-gray-700 mb-2"
+    >
+      <span className="flex items-center gap-1">
         {label} {required && <span className="text-red-500">*</span>}
       </span>
       <span className="text-gray-500 text-sm">
